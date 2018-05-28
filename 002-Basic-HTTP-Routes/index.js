9d83ec068552a1eccdde4f1d730e03cc253a5d4e
guptasoumya11 <soumyagupta9c@gmail.com>
2018-05-28 15:00:34 +0530

    new file committed

--- 002-Basic-HTTP-Routes/index.js
+++ 002-Basic-HTTP-Routes/index.js
@@ -2,108 +2,79 @@ var http = require('http');
 var url = require('url');
 var data = require('./data.json');
 
-http.createServer(function (request, response) {
-  
+http.createServer(function (request, response) {  
   
 
   const {pathname,query} = url.parse(request.url);
 
   var query_list= query.split('&');
-  var filter_query;
+  var filter_query=data;
 
   console.log("pathname:", pathname);
   console.log("queryParameters:", query);
   console.log("query-list:",query_list);
 
-  if(query_list.length==1){    
+  for(j=0;j<query_list.length;j++){
 
-    if(query_list[0].includes('name')){
-      
-      var q_parameter=query_list[0].slice(8,-3);
+    var q;
 
+    if(query_list[j].includes('=')){
+      q=query_list[j].split('=');
+      console.log(q);
+      if( q[1].includes('%22') || q[1].includes('%27')){
+      q[1] = q[1].slice(3,-3);
       
-      filter_query=data.filter( data => 
-      data['name'].includes(q_parameter)).map( data => data.name);
+      filter_query = filter_query.filter( x => x[q[0]].includes(q[1]))   
+
+      }
+      else{
+        filter_query = filter_query.filter( x => x[q[0]]==parseInt(q[1]));
+      }      
 
     }
-
-    else if(query_list[0].includes('mass')){
-
-      var value = query_list[0].slice(7);
-      var value = parseInt(value);
-
-//      console.log(value);
-
-      if( query_list[0].includes('%3E')){
-
-        filter_query = data.filter( data => data['mass'] > value).map( data => {
-          y={};
-          y.name = data.name;
-          y.mass = data.mass;
-          return y;
-        });
-
-       
-        
-      }
-
-      if( query_list[0].includes('%3C')){
-
-        filter_query = data.filter( data => data['mass'] < value).map( data => {
-          y={};
-          y.name = data.name;
-          y.mass = data.mass;
-          return y;
-        });
-
-        
-        
-      }
+    else if(query_list[j].includes('%3E')){
+      q = query_list[j].split('%3E');
+      console.log(q);
+      filter_query = filter_query.filter( x => x[q[0]]>parseInt(q[1]));
+      
     }
-    
+
+    else if(query_list[j].includes('%3C')){
+      q = query_list[j].split('%3C');
+      console.log(q);
+      filter_query = filter_query.filter( x => x[q[0]]<parseInt(q[1]));     
+    }
+   
   }
 
-  else if(query_list.length=2){
-    var q_name=query_list[0].slice(8,-3);
-    var value = query_list[1].slice(7);
-    var value = parseInt(value);
+   filter_query=filter_query.map(  filter_query => {
+     y={};
+     
+     y.name = filter_query.name;
+     y.height = filter_query.height;
+     y.mass = filter_query.mass;
+     y.hair_color = filter_query.hair_color;
+     y.skin_color = filter_query.skin_color;
+     y.eye_color = filter_query.eye_color;
+     y.gender = filter_query.gender;
+     
+      return y;
+     
+   });
 
-    if(query_list[1].includes('%3E')){
-      filter_query= data.filter( data => data['name'].includes(q_name) && data['mass']>value).map( data => {
-        y={};
-        y.name=data.name;
-        y.mass=data.mass;
-        return y;
-      });
-    }
+   console.log(filter_query);
 
-    if(query_list[1].includes('%3C')){
-      filter_query= data.filter( data => data['name'].includes(q_name) && data['mass']<value).map( data => {
-        y={};
-        y.name=data.name;
-        y.mass=data.mass;
-        return y;
-      });
-      
-    }
-  }
+   if(filter_query.length>0){
+    response.writeHead(200, {
+      'Content-type': 'text/plain'
+    });
+    response.write(' Status returned: 200');
+   }
+   else{
+     response.writeHead(404);
+     response.write("Error 404");
+   }
 
-  console.log("\n",filter_query);
-
-  if(filter_query.length>0){
-  response.writeHead(200, {
-    'Content-type': 'text/plain'
-  });
-  response.write(' Status returned: 200');
-  response.end();
-}
-else{
-  response.writeHead(400, {
-    'Content-type': 'text/plain'
-  });
-  response.write('Error 404');
-  response.end();
-}
-  
+response.end();  
 
 }).listen(7000);
