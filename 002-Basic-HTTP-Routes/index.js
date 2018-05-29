526be9e4a7dce7bbbf43113c2f0ec4c540dd0806
Udit Singh Guleria <udi.guleria@gmail.com>
2018-05-29 12:50:04 +0530

    Index modified

--- 002-Basic-HTTP-Routes/index.js
+++ 002-Basic-HTTP-Routes/index.js
@@ -1,109 +1,92 @@
 var http = require('http');
 var url = require('url');
-var data = require('./data.json');
+var data = require('./data.json');  //database file
 
 http.createServer(function (request, response) {
-  
-  
-
   const {pathname,query} = url.parse(request.url);
 
-  var query_list= query.split('&');
-  var filter_query;
+  var query_list= query.split('&'); //separate the joined queries
+  var filter_query=data;
 
   console.log("pathname:", pathname);
   console.log("queryParameters:", query);
   console.log("query-list:",query_list);
 
-  if(query_list.length==1){    
+   var l=query_list.length;
+   var i=0;
+   while(i<l)
+   {
+    if(query_list[i].includes('='))
+    {
+         var con_list=query_list[i].split('='); //separating the name and the value
+         var first = con_list[0];
+         //console.log("conlist1="+con_list[1]);
 
-    if(query_list[0].includes('name')){
-      
-      var q_parameter=query_list[0].slice(8,-3);
+         if(con_list[1].includes('%22')|con_list[1].includes('%27'))
+         { // if the value to be checked is a string
+             // console.log("Entered if");
+         second=  con_list[1].slice(3,-3); //just taking a value
+         }
+         else second=con_list[1];
+         console.log(first);   //name
+         console.log(second);  //a    
+          filter_query=filter_query.filter( x =>x[first].includes(second));
 
-      
-      filter_query=data.filter( data => 
-      data['name'].includes(q_parameter)).map( data => data.name);
+    } 
 
-    }
+      else if(query_list[i].includes('%3E')) // means >
+    {
+      var con_list=query_list[i].split('%3E'); //separating the name and the value
+         var first = con_list[0];
+         second=  con_list[1].slice(-3); //just taking a value
+         console.log(first);   //name
+       var value = parseInt(second); //the value to be found
 
-    else if(query_list[0].includes('mass')){
-
-      var value = query_list[0].slice(7);
-      var value = parseInt(value);
-
-//      console.log(value);
-
-      if( query_list[0].includes('%3E')){
-
-        filter_query = data.filter( data => data['mass'] > value).map( data => {
-          y={};
-          y.name = data.name;
-          y.mass = data.mass;
-          return y;
-        });
-
-       
-        
+       console.log(value);
+        filter_query = filter_query.filter( x => x[first] > value) ;
+   
       }
+    if( query_list[i].includes('%3C'))  // <
+      {  //means <
+          var con_list=query_list[i].split('%3C'); //separating the name and the value
+         var first = con_list[0];
+         second=  con_list[1].slice(-3); //just taking a value
+         console.log(first);   //name
+        // console.log(second);  //a
+          var value = parseInt(second); //the value to be found
 
-      if( query_list[0].includes('%3C')){
-
-        filter_query = data.filter( data => data['mass'] < value).map( data => {
+          console.log(value);
+         filter_query = filter_query.filter(x => x[first] < value).map; /*( x => {
           y={};
-          y.name = data.name;
-          y.mass = data.mass;
+          y.name = x.name;
+          y.mass = x.mass;
           return y;
-        });
-
-        
-        
+        });        */
       }
+    i++; 
     }
-    
-  }
-
-  else if(query_list.length=2){
-    var q_name=query_list[0].slice(8,-3);
-    var value = query_list[1].slice(7);
-    var value = parseInt(value);
-
-    if(query_list[1].includes('%3E')){
-      filter_query= data.filter( data => data['name'].includes(q_name) && data['mass']>value).map( data => {
-        y={};
-        y.name=data.name;
-        y.mass=data.mass;
-        return y;
-      });
-    }
-
-    if(query_list[1].includes('%3C')){
-      filter_query= data.filter( data => data['name'].includes(q_name) && data['mass']<value).map( data => {
-        y={};
-        y.name=data.name;
-        y.mass=data.mass;
-        return y;
-      });
-      
-    }
-  }
-
-  console.log("\n",filter_query);
-
-  if(filter_query.length>0){
+    var result= filter_query.map( x => {
+      y={};
+      y.name = x.name;
+      y.mass = x.mass;
+      y.height= x.height;
+  /*    response.write("Answer :"+y.prototype.toString(y.name));*/
+      return y;
+    });        
+  console.log("\n",result);
+  if(filter_query.length>0){  //if there is some output
   response.writeHead(200, {
     'Content-type': 'text/plain'
   });
-  response.write(' Status returned: 200');
+  response.write(' Status returned: 200 ->Successful');
+  //response.write("Answer :"+y.prototype.toString(y.name));
   response.end();
 }
 else{
-  response.writeHead(400, {
+  response.writeHead(400, {  //if no matching data is found
     'Content-type': 'text/plain'
   });
-  response.write('Error 404');
+  response.write('Error 404 ->Data not found');
   response.end();
 }
-  
-
-}).listen(7000);
+}).listen(2000);
