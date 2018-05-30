094752df9dd86e63e49b7ec451bf59348e5aaedb
Ujla Garg <ujlagarg@gmail.com>
2018-05-30 10:45:21 +0530

    kj;l

--- Ujla REST-Server/index.js
+++ Ujla REST-Server/index.js
@@ -7,68 +7,76 @@ http.createServer(function (request, response)
   if(!request.url.includes("/people"))
   { 
         response.writeHead(404);
-        response.write("Status:404 Not Found");
+        response.write('Status Code:'+response.statusCode.toString());
   }
   else
   {
-        response.writeHead(200, {'Content-type': 'text/plain'});
-        response.write("Status:200 OK");
+        response.writeHead(200, {'Content-type': 'text/json'});
+        response.write('Status Code:'+response.statusCode.toString());
       
         const {pathname,query} = url.parse(request.url);
         var qlist=query.split('&') //list of queries
         console.log(qlist)
         console.log("pathname", pathname);
         console.log("queryParameters", query)
-        var param   //name of parameter
-        var value   //value of parameter
-        var pos     //position of value
+        var param=[]   //name of parameter
+        var value={}  //value[i] of parameter
+        var pos     //position of value[i]
         var ans={}
       
         for(var i=0; i<qlist.length;i++)
         {
-          if(qlist[i].includes('%27')||qlist[i].includes('%22'))   //character string
+          if(qlist[i].includes('%27')||qlist[i].includes('%22'))   //character string comparision
           {
-            param=qlist[i].slice(0,qlist[i].indexOf('='))
-            //console.log(param)
+            param[i]=qlist[i].slice(0,qlist[i].indexOf('='))
+            //console.log(param[i])
             pos=qlist[i].indexOf('%')+3
-            value=qlist[i].slice(pos,-3)
-            //console.log(value)
-            data=data.filter(x => x[param].includes(value))
+            value[i]=qlist[i].slice(pos,-3)
+            //console.log(value[i])
+            data=data.filter(x => x[param[i]].includes(value[i]))
             //console.log(data.map(x => x.name))
           }
-          else if(qlist[i].includes('%3E'))  //comparasion string greater than
+          else if(qlist[i].includes('%3E'))  //comparision string greater than
           {
-            param=qlist[i].slice(0,qlist[i].indexOf('%3E'))
-            //console.log(param)
+            param[i]=qlist[i].slice(0,qlist[i].indexOf('%3E'))
+            //console.log(param[i])
             pos=qlist[i].indexOf('%')+3
-            value=qlist[i].slice(pos)
-            //console.log(value)
-            data=data.filter(x => x[param]>Number(value))
+            value[i]=qlist[i].slice(pos)
+            //console.log(value[i])
+            data=data.filter(x => x[param[i]]>Number(value[i]))
             //console.log(data.map(x => x.name))
           }
-          else if(qlist[i].includes('%3C'))   //comparasion string less than
+          else if(qlist[i].includes('%3C'))   //comparision string less than
           {
-            param=qlist[i].slice(0,qlist[i].indexOf('%3C'))
-            //console.log(param)
+            param[i]=qlist[i].slice(0,qlist[i].indexOf('%3C'))
+            //console.log(param[i])
             pos=qlist[i].indexOf('%')+3
-            value=qlist[i].slice(pos)
-            //console.log(value)
-            data=data.filter(x => x[param]<Number(value))
+            value[i]=qlist[i].slice(pos)
+            //console.log(value[i])
+            data=data.filter(x => x[param[i]]<Number(value[i]))
             //console.log(data.map(x => x.name))
           }
-          else if(qlist[i].includes('='))   //comparasion string equal to
+          else if(qlist[i].includes('='))   //comparision string equal to
           {
-            param=qlist[i].slice(0,qlist[i].indexOf('='))
-            //console.log(param)
+            param[i]=qlist[i].slice(0,qlist[i].indexOf('='))
+            //console.log(param[i])
             pos=qlist[i].indexOf('=')+1
-            value=qlist[i].slice(pos)
-            //console.log(value)
-            data=data.filter(x => x[param]=Number(value))
+            value[i]=qlist[i].slice(pos)
+            //console.log(value[i])
+            data=data.filter(x => x[param[i]]=Number(value[i]))
             //console.log(data.map(x => x.name))
           }
         }
-        ans=data.map(x => x.name) 
-        console.log("Required Records are:"+ans)
+        ans=data.map( x =>{
+          y={}
+          for(var i=0;i<param.length;i++)
+          {
+            y[param[i]]=x[param[i]]
+          }
+          return y
+        }) 
+        console.log(ans)
+        response.write("\n\nRequired Records are: "+JSON.stringify(ans))
       }
       response.end();  
 }).listen(7000);
