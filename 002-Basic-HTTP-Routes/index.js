b54e7d4ac74c1ecc7e402a594535c92ee8f372c0
guptasoumya11 <soumyagupta9c@gmail.com>
2018-05-31 11:20:05 +0530

    REST API File

--- 002-Basic-HTTP-Routes/index.js
+++ 002-Basic-HTTP-Routes/index.js
@@ -1,87 +1,49 @@
 var http = require('http');
 var url = require('url');
 var data = require('./data.json');
-
-http.createServer(function (request, response) {  
-  
+http.createServer(function (request, response) {   
 
   const {pathname,query} = url.parse(request.url);
 
   var query_list= query.split('&');
   var filter_query=data;
-  var p_name = new Array();
-
-  console.log("pathname:", pathname);
-  console.log("queryParameters:", query);
-  console.log("query-list:",query_list);
 
   for(j=0;j<query_list.length;j++){
 
+    query_list[j]= decodeURI(query_list[j]);
+
     var q;
+    q=query_list[j].split('=');
+    var offset;
+    if(query_list[j].includes('="') || query_list[j].includes("='")){
+      q[1] = q[1].slice(1,-1);      
+      filter_query = filter_query.filter( x => x[q[0]].includes(q[1]))           
+    }    
 
-    if(query_list[j].includes('=')){
-      q=query_list[j].split('=');
-      //console.log(q);
-      if( q[1].includes('%22') || q[1].includes('%27')){
-      q[1] = q[1].slice(3,-3);
-      
-      filter_query = filter_query.filter( x => x[q[0]].includes(q[1]))   
-      p_name.push(q[0]);
-      }
-      else{
-        filter_query = filter_query.filter( x => x[q[0]]==parseInt(q[1]));
-        p_name.push(q[0]);
-
-      }      
-
-    }
-    else if(query_list[j].includes('%3E')){
-      q = query_list[j].split('%3E');
-      console.log(q);
-      filter_query = filter_query.filter( x => x[q[0]]>parseInt(q[1]));
-      p_name.push(q[0]);
-
-      
-    }
-
-    else if(query_list[j].includes('%3C')){
-      q = query_list[j].split('%3C');
-      console.log(q);
-      filter_query = filter_query.filter( x => x[q[0]]<parseInt(q[1]));   
-      p_name.push(q[0]);
-  
-    }
-   
+    else{
+        q[1] = JSON.parse(q[1]);
+        if( q[1].lt != null && q[1].gt != null){
+            filter_query = filter_query.filter( x => x[q[0]]<parseInt(q[1].lt) && x[q[0]]>parseInt(q[1].gt));
+        }
+        else if( q[1].lt == null){
+            filter_query = filter_query.filter( x =>  x[q[0]]>parseInt(q[1].gt));
+        }
+        else{
+            filter_query = filter_query.filter( x =>  x[q[0]]<parseInt(q[1].lt));
+        }
+    }  
   }
-
-   filter_query = filter_query.map(  filter_query => {
-     y={};
-     
-     // y['name'] = filter_query['name'];
-     for(i =0;i<p_name.length;i++){
-       y[p_name[i]] = filter_query[p_name[i]];
-
-     }     
-      return y;
-     
-   });
-
-   console.log(filter_query);
-
    if(filter_query.length>0){
     response.writeHead(200, {
-      'Content-type': 'text/plain'
+      'Content-type': 'application/json'
     });
-    response.write(' Status returned: 200\n');
-
-    response.write("The Object returned are: "+JSON.stringify(filter_query,null,'\t'));
-    
+       response.write(JSON.stringify(filter_query,null,'\t'));    
    }
    else{
-     response.writeHead(404);
-     response.write("Error 404: Data not found");
+    response.writeHead(404, {
+      'Content-type': 'application/json'
+    });     
+     response.write(JSON.stringify(filter_query,null,'\t'));
    }
-
 response.end();  
-
 }).listen(7000);
