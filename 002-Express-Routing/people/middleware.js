816b166363bb83541fed78a8b542af4f72fe41d0
deepankar01 <deepankar01agrawal@gmail.com>
2018-06-01 09:00:16 +0530

    added basic scaffold for building

---
+++ 002-Express-Routing/people/middleware.js
@@ -0,0 +1,14 @@
+const sampleMiddleware = function (req, res, next) {
+    console.log('LOGGED')
+    next();
+  }
+
+  /* *
+  * create an error handler for error 
+  * create a route specific for error
+  * */
+
+
+
+
+module.exports = sampleMiddleware;
